be006febd27bc01d6c1e499412a5500fd1549baa
Никита Пупышев <npupyshev@icloud.com>
2016-02-03 12:25:28 +0300

    copyright notice

--- bxpatch.c
+++ bxpatch.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014, Pupyshev Nikita. <npupyshev@icloud.com>
+ * Copyright 2014-2016, Pupyshev Nikita. <npupyshev@icloud.com>
  * All rights reserved
  *
  * Redistribution and use in source and binary forms, with or without
